a5bf69de4da7ad819c850c8e6153f6ff532330cd
NaokiIshii <naoki.harry.ishii727@gmail.com>
2021-07-24 20:57:02 +0900

    コミット

---
+++ main.js
@@ -0,0 +1,24 @@
+/*const mbParallaxOne = document.getElementById("mb_parallax_one");
+const mbParallaxTwo = document.getElementById("mb_parallax_two");
+const mbParallaxThree = document.getElementById("mb_parallax_three");*/
+const addOneButton = document.getElementById("add-one-button");
+const addTwoButton = document.getElementById("add-two-button");
+const addThreeButton = document.getElementById("add-three-button");
+
+/*mbParallaxOne = function () {};
+
+mbParallaxTwo = function () {};
+
+mbParallaxThree = function () {};*/
+
+addOneButton.onclick = function () {
+  location.href = "https://disneyworld.disney.go.com/";
+};
+
+addTwoButton.onclick = function () {
+  location.href = "https://visitseattle.org/";
+};
+
+addThreeButton.onclick = function () {
+  location.href = "https://www.visitestonia.com/en/";
+};
